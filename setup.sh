4dbfaa5327bc712c486dcc9166c7a927e60fdef0
Anna Holzmann <anna.holzmann@mathema.de>
2022-03-14 10:28:08 +0100

    add wanted repos

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# 1. Change folder and dowlnoad wanted repos
+# 1. Change folder and download wanted repos
 
 cd ./src/
 
 git clone https://git.matheads.de/data-science/owlracer-ai-opensource-server.git
 git clone https://git.matheads.de/data-science/owlracer-ai-opensource-ui.git
+git clone https://git.matheads.de/data-science/owlracer-ai-group/owlracer-ai-opensource-client-ml.net.git
+git clone https://git.matheads.de/data-science/owlracer-ai-group/owlracer-ai-opensource-client-python.git
+
+#git clone https://github.com/MATHEMA-GmbH/Owl-Racer-AI-UI.git
+#git clone https://github.com/MATHEMA-GmbH/Owl-Racer-AI-Client-ML.Net.git
+#git clone https://github.com/MATHEMA-GmbH/Owl-Racer-AI-Client-Python.git
 
 # 2. Create solution file for VS
 
